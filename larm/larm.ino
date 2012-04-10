6c349ee6470b0a0721f29a4ddf49427db0f69e83
Jonas Bengtsson <jonas.b@gmail.com>
2012-04-10 23:22:31 +0200

    refactor pins

--- larm/larm.ino
+++ larm/larm.ino
@@ -1,47 +1,34 @@
+#include "pins.h"
 #include "keyboard.h"
 
-int DN         = 10; // Negativ
-int D1         = 12; // D1
-int D2         = 9;  // D2
-int D3         = 8;  // D3
+void setup() {
+  pinMode(PIN_TRAFICLIGHT_GROUND, OUTPUT);
+  pinMode(PIN_TRAFICLIGHT_RED, OUTPUT);
+  pinMode(PIN_TRAFICLIGHT_AMBER, OUTPUT);
+  pinMode(PIN_TRAFICLIGHT_GREEN, OUTPUT);
+  digitalWrite(PIN_TRAFICLIGHT_GROUND, LOW);
 
-int diod      = 4;  // Diod positiv
-int gnd       = 7;  // Diod negativ
+  pinMode(PIN_GROUND, OUTPUT);
+  digitalWrite(PIN_GROUND, LOW);
 
-int UT1        = 13; // UT1 Relä
-int UT2        = 11; // UT2 Relä
+  pinMode(PIN_KEYBOARD_DIOD, OUTPUT);
+  digitalWrite(PIN_KEYBOARD_DIOD, LOW);
 
-void setup() {                
-  pinMode(DN, OUTPUT);    
-  pinMode(D1, OUTPUT); 
-  pinMode(D2, OUTPUT);  
-  pinMode(D3, OUTPUT); 
-  digitalWrite(DN, LOW); 
-
-  pinMode(diod, OUTPUT);  
-  pinMode(gnd, OUTPUT); 
-
-  pinMode(UT1, OUTPUT);  
-  pinMode(UT2, OUTPUT);
+  pinMode(PIN_BLUELIGHT, OUTPUT);
+  pinMode(PIN_DISCOLIGHT, OUTPUT);
 
   setupKeyboard();
-
-  digitalWrite(gnd, LOW);
-  digitalWrite(diod, LOW);
 }
 
 void loop() {
   Keyboard kb = readKeyboard();
-  
+
   // act!
-  digitalWrite(diod, kb.on_1 ? HIGH : LOW);
-  digitalWrite(UT2, kb.on_2 ? HIGH : LOW);
-  
-  digitalWrite(D1, kb.on_4 ? HIGH : LOW); 
-  digitalWrite(D2, kb.off_4 ? HIGH : LOW); 
-  digitalWrite(D3, kb.off_2 ? HIGH : LOW); 
+  digitalWrite(PIN_KEYBOARD_DIOD, kb.on_1 ? HIGH : LOW);
+  digitalWrite(PIN_BLUELIGHT, kb.on_2 ? HIGH : LOW);
+
+  digitalWrite(PIN_TRAFICLIGHT_RED, kb.on_4 ? HIGH : LOW);
+  digitalWrite(PIN_TRAFICLIGHT_AMBER, kb.off_4 ? HIGH : LOW);
+  digitalWrite(PIN_TRAFICLIGHT_GREEN, kb.off_2 ? HIGH : LOW);
 }
-
-
-
 
--- larm/pins.h
+++ larm/pins.h
@@ -1,11 +1,23 @@
 #ifndef PINS_H_INCLUDED
 #define PINS_H_INCLUDED
 
+// keyboard
 const int PIN_FEED_12 = 6;
 const int PIN_FEED_34 = 0;
 const int PIN_ON_13 = 1;
 const int PIN_OFF_13 = 3;
 const int PIN_ON_24 = 2;
 const int PIN_OFF_24 = 5;
+
+const int PIN_TRAFICLIGHT_GROUND = 10;
+const int PIN_TRAFICLIGHT_RED = 9;
+const int PIN_TRAFICLIGHT_AMBER = 12;
+const int PIN_TRAFICLIGHT_GREEN = 8;
+
+const int PIN_KEYBOARD_DIOD = 4;
+const int PIN_GROUND = 7;
+
+const int PIN_BLUELIGHT = 11;
+const int PIN_DISCOLIGHT = 13;
 
 #endif // PINS_H_INCLUDED
